c98ee35271931a1f66c712e218e89f5857d5dafc
RozerinB <rozbaran@icloud.com>
2022-11-01 13:42:22 +0000

    Baics of applications

---
+++ better-life/src/mentalHealthSymptoms.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const mentalHealthSymptoms = () => {
+  return (
+    <div>mentalHealthSymptoms</div>
+  )
+}
+
+export default mentalHealthSymptoms
---
+++ better-life/src/physicalSymptoms.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const physicalSymptoms = () => {
+  return (
+    <div>physicalSymptoms</div>
+  )
+}
+
+export default physicalSymptoms
